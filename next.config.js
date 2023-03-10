afc5b34ff9831ba613f50267b1e69613b0dc0450
star <chlvkfksquf@gmail.com>
2023-03-10 17:38:41 +0900

    Modify next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  i18n: {
+    locales: ["ko"],
+    defaultLocale: "ko",
+  },
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
