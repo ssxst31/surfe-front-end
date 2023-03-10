8772cf4ed47900e52acca2f3729e4d90ec87e659
star <chlvkfksquf@gmail.com>
2023-03-10 18:15:03 +0900

    Modify style

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,26 @@
+"use client";
+
+import { useRef } from "react";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
-  return <main className=" text-red-800">메인 페이지</main>;
+  const router = useRouter();
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const goWs = () => {
+    router.push("/ws");
+  };
+
+  return (
+    <div className="flex flex-col items-center">
+      <input
+        placeholder="닉네임"
+        className="px-2 py-3 border border-gray-300 rounded-md focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 w-96"
+        ref={inputRef}
+        name="title"
+      />
+      <button onClick={goWs}>로그인</button>
+    </div>
+  );
 }
