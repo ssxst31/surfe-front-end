2016bbbdcfea3c031bbc262cf8c6e4a1a5adf456
star <chlvkfksquf@gmail.com>
2023-03-11 16:58:02 +0900

    Modify style

--- app/ws/page.tsx
+++ app/ws/page.tsx
@@ -1,44 +1,103 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import { useRecoilValue } from "recoil";
 
+import { meState } from "app/store";
 import ws from "datasources/ws";
+import { Chat } from "app/type";
 
 export default function Ws() {
-  const [message, setMessage] = useState<any>("");
-  const [chatList, setChatList] = useState<any>([]);
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  const me = useRecoilValue(meState);
+  const [inputValue, setInputValue] = useState<string>("");
+  const [chatList, setChatList] = useState<Chat[]>([]);
 
   useEffect(() => {
+    scrollRef.current?.scrollIntoView({ behavior: "smooth" });
+
     ws.on("RECEIVE_MESSAGE", (data: any) => {
       setChatList(data.chatList);
     });
-  }, [ws]);
+  }, [ws, chatList]);
 
   useEffect(() => {
     ws.emit("JOIN_ROOM", {
-      nickname: "나",
+      nickname: me,
     });
   }, []);
 
-  const postMessage = () => {
-    ws.emit("SEND_MESSAGE", {
-      message: message,
-    });
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
   };
 
-  const onChange = (e: any) => {
-    setMessage(e.target.value);
+  const postMessage = (e: React.MouseEvent<HTMLButtonElement> | React.FormEvent) => {
+    e.preventDefault();
+
+    if (!inputValue) return alert("메시지를 입력해주세요");
+
+    if (inputValue) {
+      ws.emit("SEND_MESSAGE", {
+        message: inputValue,
+        nickname: me,
+        createAt: new Date(),
+      });
+
+      setInputValue("");
+    }
   };
 
   return (
-    <div>
-      <input onChange={onChange} value={message} />
-      <div onClick={postMessage}>전송하기</div>
-      <div>
-        {chatList.map((el: any, index: any) => (
-          <div key={index}>{el}</div>
-        ))}
+    <div className="flex flex-col items-center w-full h-full">
+      <div className="w-full overflow-scroll h-96">
+        {chatList.map((chat, index) => {
+          let hours = new Date(chat.createAt).getHours();
+          let minutes = new Date(chat.createAt).getMinutes();
+
+          if (chat.nickname === me) {
+            return (
+              <div key={index} className="flex justify-end w-full">
+                <div>
+                  {hours}:{minutes}
+                </div>
+                <div className="flex flex-col">
+                  <div className="max-w-lg p-2 mb-5 break-all bg-indigo-200 rounded-lg min-w-9">{chat.message}</div>
+                </div>
+              </div>
+            );
+          } else {
+            return (
+              <div key={index} className="flex w-full space-x-4">
+                <img
+                  src="https://i.pinimg.com/236x/70/be/b2/70beb2f42ae1c4cfe7a32ec61a93c2f5.jpg"
+                  className="rounded-[50%] h-10 w-10"
+                  alt="profile"
+                />
+                <div>
+                  <div>{chat.nickname}</div>
+                  <div className="max-w-lg p-2 mb-5 break-all bg-indigo-400 rounded-lg min-w-9">{chat.message}</div>
+                </div>
+                <div>
+                  {hours}:{minutes}
+                </div>
+              </div>
+            );
+          }
+        })}
+        <div ref={scrollRef} />
       </div>
+      <form className="flex w-full" onSubmit={postMessage}>
+        <input
+          placeholder="메시지를 입력해주세요."
+          className="w-full px-2 py-3 border border-gray-300 rounded-md focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
+          onChange={onChange}
+          value={inputValue}
+        />
+        <button className="w-32 h-12 text-white bg-indigo-600 rounded-md" onClick={postMessage}>
+          전송하기
+        </button>
+      </form>
     </div>
   );
 }
