e6e611380cb0f793db421f3bf0eac86e271595bc
star <chlvkfksquf@gmail.com>
2023-03-12 18:29:02 +0900

    Modify me type

--- app/store.ts
+++ app/store.ts
@@ -3,7 +3,7 @@ import { recoilPersist } from "recoil-persist";
 
 const { persistAtom } = recoilPersist();
 
-export const meState = atom<string>({
+export const meState = atom<any>({
   key: "me",
   default: "",
   effects_UNSTABLE: [persistAtom],
