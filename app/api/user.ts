85d3eaa27d9aed889f3a463da363c4153e12e99b
star <chlvkfksquf@gmail.com>
2023-03-12 22:59:01 +0900

    Add login api

--- app/api/user.ts
+++ app/api/user.ts
@@ -8,6 +8,13 @@ export async function createUser({ email, password, nickname }: any) {
   });
 }
 
+export async function login({ email, password }: any) {
+  return await customAxios.post<null, any>(`/users/login`, {
+    email,
+    password,
+  });
+}
+
 export async function fetchProfile(token: any) {
   const tokenCookied = token.name + "=" + token.value + ";";
 
