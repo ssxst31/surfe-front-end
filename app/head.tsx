a942734085b158ad3b3da06e8b632fce093f939a
star <chlvkfksquf@gmail.com>
2023-03-13 11:21:44 +0900

    Modify haed

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default async function Head() {
+  return (
+    <>
+      <title>채팅웹</title>
+      <meta name="description" content="채팅을 위한 웹입니다." />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,6 @@ import RecoilProvider from "app/RecoilProvider";
 
 import "./globals.css";
 
-export const metadata = {
-  title: "채팅웹",
-  description: "채팅을 위한 웹입니다.",
-};
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
