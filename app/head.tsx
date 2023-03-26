a32449a66a01af01dcfc2e3f22db2eba0fe89648
star <chlvkfksquf@gmail.com>
2023-03-26 13:09:39 +0900

    Modify title

--- app/head.tsx
+++ app/head.tsx
@@ -1,8 +1,8 @@
 export default async function Head() {
   return (
     <>
-      <title>채팅웹</title>
-      <meta name="description" content="채팅을 위한 웹입니다." />
+      <title>서피</title>
+      <meta name="description" content="채팅을 위한 서비스, 서피입니다." />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
     </>
