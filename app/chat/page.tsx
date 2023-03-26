59bef90de90011bdbfd54e2d489cdbc39d201792
star <chlvkfksquf@gmail.com>
2023-03-26 18:41:37 +0900

    Clean code

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -2,17 +2,12 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 import Chat from "app/chat/components/Chat";
-import { fetchProfile } from "app/api/user";
 
 export default async function ChatPage() {
   const cookieStore = cookies();
   const token = cookieStore.get("token");
 
-  const res = await fetchProfile(token).catch((error) => {
-    return null;
-  });
-
-  if (!res) {
+  if (!token) {
     redirect("/");
   }
 
--- app/components/NonMain.tsx
+++ app/components/NonMain.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useRef } from "react";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 
 import { login } from "app/api/auth";
