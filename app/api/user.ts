53bf21c08d7ac91e5d54aca9439ebb142e5337b6
star <chlvkfksquf@gmail.com>
2023-03-28 16:27:52 +0900

    Modify post -> get

--- app/api/user.ts
+++ app/api/user.ts
@@ -3,15 +3,11 @@ import customAxios from "app/api";
 export async function fetchProfile(token: any) {
   const tokenCookied = token.name + "=" + token.value + ";";
 
-  return await customAxios.post<null, any>(
-    "/user/profile",
-    {},
-    {
-      headers: {
-        cookies: tokenCookied,
-      },
+  return await customAxios.get<null, any>("/user/profile", {
+    headers: {
+      cookies: tokenCookied,
     },
-  );
+  });
 }
 
 export async function fetchUserList() {
