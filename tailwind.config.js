07e8512e1b0774b4300b7575984e4d28e68fae19
star <chlvkfksquf@gmail.com>
2023-03-29 23:35:50 +0900

    Modify tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./layouts/**/*.{js,ts,jsx,tsx}",
+    "./stories/**/*.{js,ts,jsx,tsx}",
+    "src/**/*.{js,jsx,ts,tsx}",
+  ],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "-2xl": { max: "1535px" },
+        // => @media (min-width: 640px) { ... }
+
+        "-xl": { max: "1280px" },
+        // => @media (min-width: 768px) { ... }
+
+        "-lg": { max: "1024px" },
+        // => @media (min-width: 1024px) { ... }
+
+        "-md": { max: "768px" },
+        // => @media (min-width: 1280px) { ... }
+
+        "-sm": { max: "640px" },
+        // => @media (min-width: 1536px) { ... }
+      },
+    },
   },
   plugins: [],
 };
