88a24a5571d9606af85c372480006be426928952
star <chlvkfksquf@gmail.com>
2023-03-29 14:07:29 +0900

    test

--- components/Main.tsx
+++ components/Main.tsx
@@ -2,7 +2,7 @@ import Total from "components/NonMain";
 import Main2 from "components/Main2";
 
 export default function Main({ profile }: any) {
-  return profile ? (
+  return 0 ? (
     <div>
       {`${profile.nickname}(${profile.email})님 안녕하세요`}
       <Main2 profile={profile} />
