7b22165f52db53101cbb7bf879dc9b39d904b461
star <chlvkfksquf@gmail.com>
2023-04-01 01:42:08 +0900

    Modify userList style

--- components/UserList.tsx
+++ components/UserList.tsx
@@ -23,22 +23,33 @@ export default function UserList() {
   return (
     <div className="flex flex-col">
       <KakaoMap userListCount={userList.length} myLat={Number(me?.lat)} myLng={Number(me?.lng)} />
-      <div className="flex flex-col mt-8 -sm:px-3">
+      <div className="flex flex-col mt-8 space-y-3 -sm:px-3">
         <div className="text-lg font-extrabold">마음에 드는 사람에게 말을 걸어보세요!</div>
         {userList.map((item: any) => (
-          <div key={item.id} className="flex items-center">
-            <img src="/defaultProfile.png" className="rounded-[50%] h-22 w-22 shadow-2xl" alt="profile" />
-            <div className="mr-4 text-lg">{item.nickname}</div>
-            <Link
-              href={{
-                pathname: "/privateChat",
-                query: { room: item.id },
-              }}
-            >
-              <button>1대1채팅하기</button>
-            </Link>
+          <div key={item.id}>
+            <div className="flex justify-between">
+              <div className="flex">
+                <img
+                  src="https://i.pinimg.com/236x/70/be/b2/70beb2f42ae1c4cfe7a32ec61a93c2f5.jpg"
+                  className="rounded-[50%] h-16 w-16 shadow-2xl mr-2"
+                  alt="profile"
+                />
+                <div className="mr-4 text-lg">{item.nickname}</div>
+              </div>
+              <Link
+                href={{
+                  pathname: "/privateChat",
+                  query: { room: item.id },
+                }}
+              >
+                <button className="w-full px-4 py-2 text-xs font-medium text-white bg-blue-500 rounded-lg hover:bg-blue-600">
+                  1대1채팅하기
+                </button>
+              </Link>
+            </div>
           </div>
         ))}
+
         <div className="text-lg font-extrabold">대화할 사람이 없나요?</div>
         <Link
           href={{
