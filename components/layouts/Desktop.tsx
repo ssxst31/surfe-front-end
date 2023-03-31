9cc3e3d1e07cde53382fe160427ba384d9f7252a
star <chlvkfksquf@gmail.com>
2023-03-31 15:45:04 +0900

    Modify header style

--- components/layouts/Desktop.tsx
+++ components/layouts/Desktop.tsx
@@ -1,15 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
+import Link from "next/link";
+
+import { logOut } from "pages/api/auth";
 import Header from "components/layouts/Header";
 import Footer from "components/layouts/Footer";
+import useMe from "hooks/useMe";
 
 interface ResponsiveContainerProps {
   children: JSX.Element | JSX.Element[];
 }
 
 export default function ResponsiveContainer({ children }: ResponsiveContainerProps) {
+  const me = useMe();
+
+  const [isSidebar, setIsSidebar] = useState<boolean>(false);
+
+  const signOut = async () => {
+    await logOut();
+
+    window.location.href = "/";
+  };
+
   return (
     <div>
-      <Header />
+      {isSidebar && (
+        <div
+          className={`hidden -sm:block fixed top-0 left-0 z-10 w-full h-full bg-black bg-opacity-50`}
+          style={{
+            transition: "all 0.3s ease-out",
+          }}
+          onClick={() => {
+            setIsSidebar((prev) => !prev);
+          }}
+        >
+          <div
+            className="absolute top-0 bottom-0 left-0 z-10 w-64 h-full p-4 bg-white"
+            style={{
+              transition: "all 0.3s ease-out",
+              left: isSidebar ? "0" : "100%",
+            }}
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+          >
+            <span className="text-3xl">서피</span>
+            {me ? (
+              <div className="text-black">
+                <div className="flex items-center">
+                  <img
+                    src="https://i.pinimg.com/236x/70/be/b2/70beb2f42ae1c4cfe7a32ec61a93c2f5.jpg"
+                    className="rounded-[50%] h-10 w-10 mr-2"
+                    alt="profile"
+                  />
+                  <div>{me.nickname}</div>
+                </div>
+                <button onClick={signOut} className="py-2 text-white bg-blue-400 rounded-lg w-36">
+                  로그아웃
+                </button>
+              </div>
+            ) : (
+              <div className="flex space-x-3">
+                <Link href="/" className="w-36">
+                  <button
+                    className="w-full py-2 text-white bg-blue-400 rounded-lg"
+                    onClick={() => {
+                      setIsSidebar(false);
+                    }}
+                  >
+                    로그인
+                  </button>
+                </Link>
+                <Link href="/signup" className="w-36">
+                  <button
+                    className="w-full py-2 text-white bg-blue-400 rounded-lg"
+                    onClick={() => {
+                      setIsSidebar(false);
+                    }}
+                  >
+                    회원가입
+                  </button>
+                </Link>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+      <Header setIsSidebar={setIsSidebar} me={me} />
       <div className="w-full h-24" />
       <div className="max-w-4xl mx-auto">{children}</div>
       <Footer />
--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import Link from "next/link";
 
-import useMe from "hooks/useMe";
 import { logOut } from "pages/api/auth";
 
-export default function Header() {
-  const me = useMe();
-
+export default function Header({ setIsSidebar, me }: any) {
   const signOut = async () => {
     await logOut();
 
@@ -18,16 +15,32 @@ export default function Header() {
       <Link href="/">
         <img src="/logo.png" className="h-9 w-9" />
       </Link>
-      {me ? (
-        <div className="text-white">
-          {`${me.nickname} (${me.email})`}
-          <button onClick={signOut}>로그아웃</button>
-        </div>
-      ) : (
-        <Link href="/">
-          <button className="p-1 text-white w-14">로그인</button>
-        </Link>
-      )}
+      <div className="flex">
+        {me ? (
+          <div className="flex items-center text-white -sm:hidden">
+            <img
+              src="https://i.pinimg.com/236x/70/be/b2/70beb2f42ae1c4cfe7a32ec61a93c2f5.jpg"
+              className="rounded-[50%] h-10 w-10 mr-2"
+              alt="profile"
+            />
+            {me.nickname}
+            <button onClick={signOut}>로그아웃</button>
+          </div>
+        ) : (
+          <Link href="/">
+            <button className="block p-1 text-white w-14 -sm:hidden">로그인</button>
+          </Link>
+        )}
+        <img
+          src="/hamburgerMenu.png"
+          width={20}
+          height={20}
+          className="hidden -sm:block"
+          onClick={() => {
+            setIsSidebar((prev: boolean) => !prev);
+          }}
+        />
+      </div>
     </div>
   );
 }
