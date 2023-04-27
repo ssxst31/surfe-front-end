a44850786b567ef2bde59a6327505baca4d25675
star <chlvkfksquf@gmail.com>
2023-04-27 15:44:28 +0900

    Add ghost div

--- src/components/layouts/MobileBottomNav.tsx
+++ src/components/layouts/MobileBottomNav.tsx
@@ -11,45 +11,48 @@ export default function MobileBottomNav() {
   const activeNavPage = !notPublicPages.includes(pageRootName);
 
   return activeNavPage ? (
-    <div className="fixed w-full bg-white z-[2] -lg:px-5 shadow-xl text-center py-3 bottom-0 lg:hidden  border-t border-solid border-gray-200">
-      <ul className="flex justify-between w-full max-w-4xl mx-auto">
-        <li className="w-20 text-xs">
-          <Link
-            href={{
-              pathname: "/",
-            }}
-          >
-            홈
-          </Link>
-        </li>
-        <li className="w-20 text-xs">
-          <Link
-            href={{
-              pathname: "/my/friendList",
-            }}
-          >
-            친구관리
-          </Link>
-        </li>
-        <li className="w-20 text-xs">
-          <Link
-            href={{
-              pathname: "/my/chatList",
-            }}
-          >
-            채팅목록
-          </Link>
-        </li>
-        <li className="w-20 text-xs">
-          <Link
-            href={{
-              pathname: "/setting/profile",
-            }}
-          >
-            내 프로필 수정
-          </Link>
-        </li>
-      </ul>
+    <div>
+      <div className="h-[41px]" />
+      <div className="fixed w-full bg-white z-[2] -lg:px-5 shadow-xl text-center py-3 bottom-0 lg:hidden  border-t border-solid border-gray-200">
+        <ul className="flex justify-between w-full max-w-4xl mx-auto">
+          <li className="w-20 text-xs">
+            <Link
+              href={{
+                pathname: "/",
+              }}
+            >
+              홈
+            </Link>
+          </li>
+          <li className="w-20 text-xs">
+            <Link
+              href={{
+                pathname: "/my/friendList",
+              }}
+            >
+              친구관리
+            </Link>
+          </li>
+          <li className="w-20 text-xs">
+            <Link
+              href={{
+                pathname: "/my/chatList",
+              }}
+            >
+              채팅목록
+            </Link>
+          </li>
+          <li className="w-20 text-xs">
+            <Link
+              href={{
+                pathname: "/setting/profile",
+              }}
+            >
+              내 프로필 수정
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
   ) : (
     <></>
