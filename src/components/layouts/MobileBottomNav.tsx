a302a599afb4760e49efd2599695936bd7b4653c
star <chlvkfksquf@gmail.com>
2023-04-27 14:44:21 +0900

    Add activeNavPage

--- src/components/layouts/MobileBottomNav.tsx
+++ src/components/layouts/MobileBottomNav.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function MobileBottomNav() {
-  return (
+  const router = useRouter();
+  const currentPage = router.pathname;
+  const notPublicPages = ["/", "privateChat", "everyChat"];
+  const pageRootName = currentPage !== "/" ? currentPage.slice(1).split("/")[0] : "/";
+
+  const activeNavPage = !notPublicPages.includes(pageRootName);
+
+  return activeNavPage ? (
     <div className="fixed w-full bg-white z-[2] -lg:px-5 shadow-xl text-center py-3 bottom-0 lg:hidden  border-t border-solid border-gray-200">
       <ul className="flex justify-between w-full max-w-4xl mx-auto">
         <li className="w-20 text-xs">
@@ -43,5 +51,7 @@ export default function MobileBottomNav() {
         </li>
       </ul>
     </div>
+  ) : (
+    <></>
   );
 }
