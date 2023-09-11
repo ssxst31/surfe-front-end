d2f200381670634ca7039ff27bce07e0d03ac77d
star <chlvkfksquf@gmail.com>
2023-09-11 22:02:17 +0900

    Add UserSkeleton

---
+++ src/components/page/explore/UserSkeleton.tsx
@@ -0,0 +1,11 @@
+export default function UserSkeleton() {
+  return (
+    <div className="animate-pulse flex justify-between py-3">
+      <div className="flex">
+        <div className="w-14 h-14 bg-gray-100 rounded-[50%] mr-2" />
+        <div className="h-6 bg-gray-100 w-32 rounded-lg" />
+      </div>
+      <div className="bg-gray-100 w-20 rounded-lg h-8 px-4" />
+    </div>
+  );
+}
