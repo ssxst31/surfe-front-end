9a967858d2eb85eb03badd37253fcd8aed880db8
star <chlvkfksquf@gmail.com>
2023-10-11 20:34:00 +0900

    Clean middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,21 @@
 import { NextRequest } from "next/server";
-
 import { withAuth, withoutAuth } from "middlewares/auth";
 
 export async function middleware(request: NextRequest) {
-  if (
-    request.nextUrl.pathname.startsWith("/every-chat") ||
-    request.nextUrl.pathname.startsWith("/setting") ||
-    request.nextUrl.pathname.startsWith("/private-chat") ||
-    request.nextUrl.pathname.startsWith("/my") ||
-    request.nextUrl.pathname.startsWith("/explore")
-  ) {
+  const { nextUrl } = request;
+  const path = nextUrl.pathname;
+
+  const authRequiredPaths = ["/every-chat", "/setting", "/private-chat", "/my", "/explore"];
+
+  if (authRequiredPaths.some((requiredPath) => path.startsWith(requiredPath))) {
     return await withAuth(request);
   }
 
-  if (request.nextUrl.pathname.startsWith("/signup") || request.nextUrl.pathname === "/") {
+  const authNotRequiredPaths = ["/signup", "/"];
+
+  if (authNotRequiredPaths.includes(path)) {
     return await withoutAuth(request);
   }
+
+  return request;
 }
