5aa234749a218b54ea94a97cb16630f2f0c13b55
star <chlvkfksquf@gmail.com>
2023-10-11 20:34:00 +0900

    Revert "Clean middleware"
    This reverts commit 9a967858d2eb85eb03badd37253fcd8aed880db8.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,19 @@
 import { NextRequest } from "next/server";
+
 import { withAuth, withoutAuth } from "middlewares/auth";
 
 export async function middleware(request: NextRequest) {
-  const { nextUrl } = request;
-  const path = nextUrl.pathname;
-
-  const authRequiredPaths = ["/every-chat", "/setting", "/private-chat", "/my", "/explore"];
-
-  if (authRequiredPaths.some((requiredPath) => path.startsWith(requiredPath))) {
+  if (
+    request.nextUrl.pathname.startsWith("/every-chat") ||
+    request.nextUrl.pathname.startsWith("/setting") ||
+    request.nextUrl.pathname.startsWith("/private-chat") ||
+    request.nextUrl.pathname.startsWith("/my") ||
+    request.nextUrl.pathname.startsWith("/explore")
+  ) {
     return await withAuth(request);
   }
 
-  const authNotRequiredPaths = ["/signup", "/"];
-
-  if (authNotRequiredPaths.includes(path)) {
+  if (request.nextUrl.pathname.startsWith("/signup") || request.nextUrl.pathname === "/") {
     return await withoutAuth(request);
   }
-
-  return request;
 }
