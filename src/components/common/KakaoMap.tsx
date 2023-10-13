c386832556970136571d11a74d94d2cbe82c108d
star <chlvkfksquf@gmail.com>
2023-10-14 01:52:39 +0900

    Clean kakao map

--- src/components/common/KakaoMap.tsx
+++ src/components/common/KakaoMap.tsx
@@ -1,15 +1,10 @@
 import React, { useEffect, useRef } from "react";
-import Script from "next/script";
-import Head from "next/head";
-
-const filteredDistance = 5;
 
 interface KakaoMapProps {
   userListCount: number;
   myLat: number;
   myLng: number;
 }
-
 declare global {
   interface Window {
     kakao: any;
@@ -22,58 +17,55 @@ const NEXT_PUBLIC_KAKAO_KEY = process.env.NEXT_PUBLIC_KAKAO_APP_KEY;
 const KakaoMap = ({ userListCount, myLat, myLng }: KakaoMapProps) => {
   const containerRef = useRef<HTMLDivElement>(null);
 
-  const initMap = () => {
-    if (containerRef.current) {
-      const map = new window.kakao.maps.Map(containerRef.current, {
-        center: new window.kakao.maps.LatLng(myLat, myLng),
-        level: 8,
-      });
-      const createContent = () => {
-        return `<div style="color:white;text-shadow: -1px 0 #000, 0 1px #000, 1px 0 #000, 0 -1px #000;">${`${filteredDistance}키로내 ${userListCount}명을 발견했어요⚡`}</div>`;
-      };
+  const createCustomOverlay = (map: any) => {
+    const { kakao } = window;
+    const content = `<div style="color:white;text-shadow: -1px 0 #000, 0 1px #000, 1px 0 #000, 0 -1px #000;">${`${5}키로내 ${userListCount}명을 발견했어요⚡`}</div>`;
+    new kakao.maps.CustomOverlay({
+      map,
+      content,
+      position: new kakao.maps.LatLng(myLat, myLng),
+      xAnchor: 0.5,
+      yAnchor: 0.5,
+    });
+  };
 
-      new kakao.maps.CustomOverlay({
-        map: map,
-        content: createContent(),
-        position: new kakao.maps.LatLng(myLat, myLng),
-        xAnchor: 0.5,
-        yAnchor: 0.5,
-      });
-
-      new kakao.maps.Circle({
-        map: map,
-        center: new kakao.maps.LatLng(myLat, myLng),
-        radius: filteredDistance * 1000,
-        fillColor: "#CFE7FF",
-        fillOpacity: 0.5,
-        strokeWeight: 3,
-        strokeColor: "#75B8FA",
-        strokeOpacity: 0.9,
-        strokeStyle: "solid",
-      });
-    }
+  const createCircle = (map: any) => {
+    const { kakao } = window;
+    new kakao.maps.Circle({
+      map,
+      center: new kakao.maps.LatLng(myLat, myLng),
+      radius: 5 * 1000,
+      fillColor: "#CFE7FF",
+      fillOpacity: 0.5,
+      strokeWeight: 3,
+      strokeColor: "#75B8FA",
+      strokeOpacity: 0.9,
+      strokeStyle: "solid",
+    });
   };
 
   useEffect(() => {
-    if (window?.kakao) {
-      initMap();
+    const kakaoMapScript = document.createElement("script");
+    kakaoMapScript.async = true;
+    kakaoMapScript.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${NEXT_PUBLIC_KAKAO_KEY}&autoload=false`;
+    document.head.appendChild(kakaoMapScript);
+  }, []);
+
+  useEffect(() => {
+    if (containerRef.current && window?.kakao) {
+      window.kakao.maps.load(() => {
+        const { kakao } = window;
+        const map = new kakao.maps.Map(containerRef.current, {
+          center: new kakao.maps.LatLng(myLat, myLng),
+          level: 8,
+        });
+        createCustomOverlay(map);
+        createCircle(map);
+      });
     }
-  }, [initMap, myLat, myLng, userListCount]);
+  }, [myLat, myLng, userListCount]);
 
-  return (
-    <React.Fragment>
-      <Script
-        src={`https://dapi.kakao.com/v2/maps/sdk.js?appkey=${NEXT_PUBLIC_KAKAO_KEY}&autoload=false`}
-        onLoad={() => window.kakao.maps.load(initMap)}
-      />
-      <Head>
-        <link rel="preconnect" href="https://dapi.kakao.com" />
-        <link rel="dns-prefetch" href="https://dapi.kakao.com" />
-      </Head>
-
-      <div id="map" ref={containerRef} className="shadow-md h-[500px] -lg:h-96 w-full" />
-    </React.Fragment>
-  );
+  return <div ref={containerRef} className="shadow-md h-[500px] -lg:h-96 w-full" />;
 };
 
-export default React.memo(KakaoMap);
+export default KakaoMap;
